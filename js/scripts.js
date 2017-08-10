7a25cb669ff0b80239380e9a2d028671a698fd68
lainey rokkan and rebecca mcintosh <rebecca@>
2017-08-09 17:10:09 -0700

    last ditch effort on appointments

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  debugger;
   $("#formOne").submit(function(event) {
-    var name = $("input#person1").val();
-    var appointment = $("input#appointment").val();
-    var date = $("input#date").val();
-    var time1 = $("input#time1").val();
-    var time2 = $("input#time2").val();
+    var name = $("#person1").val();
+    var appointment = $("#appointment").val();
+    var date = $("#date").val();
+    alert("date")
+    var time1 = $("#time1").val();
+    var time2 = $("#time2").val();
+    alert("f");
     //var book = list(name, appointment, date, time);
     $(".person1").text(name);
     $(".appointment").text(appointment);
@@ -13,8 +14,9 @@ $(document).ready(function() {
     $(".time1").text(time1);
     $(".time2").text(time2);
 
-    //$(".btn").submit(book);
+  //  $(".btn").submit(book);
     $("#story").show();
     event.preventDefault();
+    // alert("f");
   });
 });
